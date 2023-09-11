c9b8337ecbc72d3c0b885bb9e7db90aeef3f1102
Filip Krunic <fkrunic9@gmail.com>
2023-09-10 18:06:58 -0700

    mobile design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
-
+import BulletPoint from './components/BulletPoint.vue';
 
 onMounted(() => {
   document.body.className = 'font-roboto'
@@ -8,7 +8,40 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center min-h-screen">
-    <p class="text-3xl font-bold underline">Hello world!</p>
+
+  <!-- Page Layout -->
+  <div class="flex flex-col justify-start items-center min-h-screen">
+    <img src="./assets/illustration-sign-up-mobile.svg" alt="banner">
+
+    <!-- Content -->
+    <div class="flex flex-col gap-6 m-6">
+      <p class="text-[30pt] font-bold -mb-2">Stay updated!</p>
+      <p>Join 60,000+ product managers receiving monthly updates on:</p>
+
+      <!-- List -->
+      <div class="flex flex-col gap-2">
+        <BulletPoint 
+        :content="'Product discovery and building what matters'"
+        ></BulletPoint>
+        <BulletPoint 
+        :content="'Measuring to ensure updates are a success'"
+        ></BulletPoint>        
+        <BulletPoint 
+        :content="'And much more!'"
+        ></BulletPoint>  
+      </div>              
+
+      <!-- Email Input -->
+      <div class="flex flex-col mt-2 gap-2">
+        <p class="text-xs font-bold">Email address</p>
+        <input 
+          class="w-full p-4 outline-none rounded-lg border-solid border-[1px] border-grey/50"
+          type="text" 
+          placeholder="email@company.com">
+      </div>
+
+      <!-- Subscribe Button -->
+      <p class="w-full p-4 text-center rounded-lg font-bold text-white bg-dark-slate-gray">Subscribe to monthly newsletter</p>
+    </div>
   </div>
 </template>
---
+++ src/components/BulletPoint.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ content: string }>()
+</script>
+
+<template>
+  <div class="flex flex-row gap-4">
+    <img class="w-5 h-5" src="../assets/icon-list.svg">
+    <p>{{ content }}</p>
+  </div>
+</template>
